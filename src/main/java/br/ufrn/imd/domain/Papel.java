f522dac0a9feed072efaad0a54b233fde4def181
Rita Lopes <rita.lopes@smartsuite.imd.ufrn.br>
2023-10-12 01:59:22 -0300

    create crud of Papel

--- src/main/java/br/ufrn/imd/domain/Papel.java
+++ src/main/java/br/ufrn/imd/domain/Papel.java
@@ -2,6 +2,7 @@ package br.ufrn.imd.domain;
 
 import java.util.Objects;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,30 +11,36 @@ import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class Papel {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_ID")
-	@SequenceGenerator(name="SEQ_ID", sequenceName="id_seq_papel", allocationSize=1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_ID")
+	@SequenceGenerator(name = "SEQ_ID", sequenceName = "id_seq_papel", allocationSize = 1)
 	private Long id;
-	
+
+	@Nonnull
 	private String descricao;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(descricao, id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -44,5 +51,5 @@ public class Papel {
 			return false;
 		Papel other = (Papel) obj;
 		return Objects.equals(descricao, other.descricao) && id == other.id;
-	}	
+	}
 }
--- src/main/java/br/ufrn/imd/services/PapelService.java
+++ src/main/java/br/ufrn/imd/services/PapelService.java
@@ -1,9 +1,18 @@
 package br.ufrn.imd.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.imd.domain.Papel;
@@ -14,10 +23,84 @@ public class PapelService {
 
 	@Autowired
 	private PapelRepository repository;
-	
-	@RequestMapping("/papeis")
-	public List<Papel> getUsers() {
-		return (List<Papel>) repository.findAll();
+
+	@GetMapping("/papeis")
+	public ResponseEntity<List<Papel>> getAllPapeis() {
+		System.out.println("getAllPapeis");
+
+		try {
+			List<Papel> papeis = new ArrayList<Papel>();
+			repository.findAll().forEach(papeis::add);
+			if (papeis.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(papeis, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@GetMapping("/papeis/{id}")
+	public ResponseEntity<Papel> getPapelById(@PathVariable("id") long id) {
+		System.out.println("getPapelById");
+		Optional<Papel> papelDB = repository.findById(id);
+		if (papelDB.isPresent()) {
+			return new ResponseEntity<>(papelDB.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping("/papeis")
+	public ResponseEntity<Papel> createPapel(@RequestBody Papel papel) {
+		System.out.println("addPapel");
+		try {
+			Papel papelDB = repository.save(papel);
+			return new ResponseEntity<>(papelDB, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@PutMapping("/papeis/{id}")
+	public ResponseEntity<Papel> updatePapel(@PathVariable("id") long id, @RequestBody Papel papel) {
+		System.out.println("updatePapel");
+
+		Optional<Papel> papelInDB = repository.findById(id);
+		if (papelInDB.isPresent()) {
+			Papel papel2 = papelInDB.get();
+			papel2.setId(papel.getId());
+			papel2.setDescricao(papel.getDescricao());
+			return new ResponseEntity<>(repository.save(papel2), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@DeleteMapping("/papeis/{id}")
+	public ResponseEntity<HttpStatus> deletePapel(@PathVariable("id") long id) {
+		System.out.println("deletePapeis");
+
+		try {
+			repository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/papeis")
+	public ResponseEntity<HttpStatus> deleteAllPapeis() {
+		System.out.println("deleteAllPapeis");
+		try {
+			repository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
